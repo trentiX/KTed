dcf37928051e5eeb3130d2ac7c7db347fa973d6c
Kaisar <130845163+trentiX@users.noreply.github.com>
2024-01-28 19:44:56 +0600

    EasterEggFX

--- kted/Assets/Scripts/EasterEggs/EasterEggPickUp.cs
+++ kted/Assets/Scripts/EasterEggs/EasterEggPickUp.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class EasterEggPickUp : MonoBehaviour
 {
+    [SerializeField] private GameObject pickUpFX;
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
@@ -12,7 +14,9 @@ public class EasterEggPickUp : MonoBehaviour
             if (easterEggManager != null)
             {
                 easterEggManager.IncreaseEasterEggCount();
+                GameObject fx = Instantiate(pickUpFX, transform.position, quaternion.identity);
                 Destroy(gameObject); // Опционально: уничтожить пасхалку после подбора
+                Destroy(fx, 1.9f);
             }
         }
     }
