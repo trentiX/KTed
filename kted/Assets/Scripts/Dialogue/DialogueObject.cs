73e1d7d9708e3220fbf53cd06050e6586f3bef8b
Kaisar <130845163+trentiX@users.noreply.github.com>
2024-09-05 23:36:49 +0500

    base of response dialogue

--- kted/Assets/Scripts/Dialogue/DialogueObject.cs
+++ kted/Assets/Scripts/Dialogue/DialogueObject.cs
@@ -5,9 +5,13 @@ using UnityEngine;
 public class DialogueObject : ScriptableObject
 {
     [SerializeField][TextArea] private string[] dialogueRus;
+    [SerializeField] private Response[] _responses;
+    
     [SerializeField][TextArea] private string[] dialogueKaz;
+    
     [SerializeField] public Sprite sprite;
     public Sprite Sprite => sprite;
     public string[] DialogueRus => dialogueRus;
+    public Response[] Responses => _responses;
     public string[] DialogueKaz => dialogueKaz;
 }
---
+++ kted/Assets/Scripts/Dialogue/ResponseHandler.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class ResponseHandler : MonoBehaviour
+{
+    public void ShowResponses(Response[] responses)
+    {
+        
+    }
+}
