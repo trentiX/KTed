d8ec4d7c04597a9fc1ff6719385592865b437c3c
Kaisar <130845163+trentiX@users.noreply.github.com>
2024-09-27 23:40:03 +0500

    Browser UI and tabs logic

---
+++ kted/Assets/Scripts/SmartPhone/Browser.cs
@@ -0,0 +1,159 @@
+using System;
+using System.Collections.Generic;
+using DG.Tweening;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+using Image = UnityEngine.UI.Image;
+
+public class Browser : MonoBehaviour
+{
+    // Serialization
+    [Header("Tabs")]
+    [SerializeField] private GameObject tabsbox;
+    [SerializeField] private GameObject tabsTemplate;
+    [SerializeField] private TextMeshProUGUI url;
+    
+    [Header("Shortcuts")] 
+    [SerializeField] private GameObject[] sites;
+    [SerializeField] private GameObject mainPage;
+
+    // Variables
+    private List<GameObject> _tabsOpened = new List<GameObject>();
+    private Dictionary<GameObject, GameObject> shortcuts = new Dictionary<GameObject, GameObject>();
+    private Tweener browserAnimation;
+    private GameObject openedPage;
+    
+    // Code 
+    private void Start()
+    {
+        openedPage = mainPage;
+    }
+
+    private void AddNewTab(GameObject tab, ShortCutButton shortCutButton)
+    {
+        if (tab == null)
+        {
+            Debug.Log("tab is null");
+            return;
+        }
+        if (_tabsOpened.Count > 3) return;
+
+        GameObject newTab = Instantiate(tabsTemplate, tabsbox.transform);
+        newTab.SetActive(true);
+        _tabsOpened.Add(newTab);
+
+        ChangeAlpha(0, newTab);
+
+        newTab.GetComponentInChildren<TextMeshProUGUI>().text
+            = tab.GetComponentInChildren<TextMeshProUGUI>().text;
+        
+        EventTrigger eventTrigger = newTab.AddComponent<EventTrigger>();
+        EventTrigger.Entry onClick = new EventTrigger.Entry()
+        {
+            eventID = EventTriggerType.PointerClick
+        };
+        
+        EventTrigger.Entry onEnter = new EventTrigger.Entry()
+        {
+            eventID = EventTriggerType.PointerEnter
+        };
+        
+        EventTrigger.Entry onExit = new EventTrigger.Entry()
+        {
+            eventID = EventTriggerType.PointerExit
+        };
+
+        onClick.callback.AddListener((eventData) => OpenPage(tab, shortCutButton));
+        onEnter.callback.AddListener((AbstractEventData) =>
+        {
+            ChangeAlpha(0.5f, newTab);
+
+        });
+        onExit.callback.AddListener((AbstractEventData) =>
+        {
+            ChangeAlpha(0, newTab);
+        });
+        
+        eventTrigger.triggers.Add(onClick);
+        eventTrigger.triggers.Add(onEnter);
+        eventTrigger.triggers.Add(onExit);
+    }
+
+    private void OpenPage(GameObject tab, ShortCutButton shortCutButton)
+    {
+        if (browserAnimation.IsActive()) return;
+        shortcuts.TryGetValue(tab, out var tabsSite);
+        {
+            if (tabsSite == null)
+            {
+                Debug.Log("tabsSite is null");
+                return;
+            }
+
+            url.text = shortCutButton.url;
+            tabsSite.SetActive(true);
+            tabsSite.GetComponent<CanvasGroup>().alpha = 0;
+            
+            browserAnimation = openedPage.GetComponent<CanvasGroup>().
+                DOFade(0, 0.2f).OnComplete((() =>
+                {
+                    browserAnimation = tabsSite.GetComponent<CanvasGroup>().DOFade(1, 0.2f);
+
+                    openedPage.GetComponent<CanvasGroup>().interactable = false;
+                    openedPage.GetComponent<CanvasGroup>().blocksRaycasts = false;
+                    
+                    openedPage = tabsSite;
+                    
+                    tabsSite.GetComponent<CanvasGroup>().interactable = true;
+                    tabsSite.GetComponent<CanvasGroup>().blocksRaycasts = true;
+                }));
+        }
+    }
+    
+    public void OnShortcutClick(ShortCutButton shortcut)
+    {
+        AddNewTab(shortcut.gameObject, shortcut);
+
+        if (shortcut.site == null)
+        {
+            Debug.Log("shortcut site is null");
+            return;
+        }
+        
+        if (shortcuts.ContainsKey(shortcut.gameObject)) return;
+        shortcuts.Add(shortcut.gameObject, shortcut.site);
+    }
+
+    public void OnHomeClick(GameObject mainPage)
+    {
+        if (browserAnimation.IsActive()) return;
+
+        mainPage.SetActive(true);
+        mainPage.GetComponent<CanvasGroup>().alpha = 0;
+        url.text = "https//www.kted.net/main_page";
+            
+        browserAnimation = openedPage.GetComponent<CanvasGroup>().
+            DOFade(0, 0.3f).OnComplete((() =>
+            {
+                browserAnimation = mainPage.GetComponent<CanvasGroup>().DOFade(1, 0.5f);
+
+                openedPage.GetComponent<CanvasGroup>().interactable = false;
+                openedPage.GetComponent<CanvasGroup>().blocksRaycasts = false;
+                    
+                openedPage = mainPage;
+                    
+                mainPage.GetComponent<CanvasGroup>().interactable = true;
+                mainPage.GetComponent<CanvasGroup>().blocksRaycasts = true;
+            }));
+    }
+    
+    private void ChangeAlpha(float value, GameObject chat)
+    {
+        var color = chat.GetComponent<Image>().color;
+        color.a = value;
+        chat.GetComponent<Image>().color = color;
+    }
+}
---
+++ kted/Assets/Scripts/SmartPhone/ShortCutButton.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ShortCutButton : MonoBehaviour
+{
+    [SerializeField] public GameObject site;
+    [SerializeField] public string url;
+}
